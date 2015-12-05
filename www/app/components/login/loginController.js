d536c5285a872ca76e51ab911ce6f221a8ecde9a
Ivelin Penchev <ivelinpenchev@gmail.com>
2015-12-05 12:43:14 +0000

    Added sqlite as database access layer

--- www/app/components/login/loginController.js
+++ www/app/components/login/loginController.js
@@ -3,7 +3,7 @@ app.controller('loginController', ['$scope', '$cordovaGeolocation',
 	function ($scope, $cordovaGeolocation) {
 		$scope.greeting = 'Hola Amigo! It kind of works!';
 
-		var posOptions = { timeout: 10000, enableHighAccuracy: false };
+		var posOptions = { timeout: 10000, enableHighAccuracy: true };
 		$cordovaGeolocation
 			.getCurrentPosition(posOptions)
 			.then(function (position) {
--- www/app/main.js
+++ www/app/main.js
@@ -10,3 +10,10 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 			controller: 'loginController'
 		})
 }]);
+
+app.run(['$cordovaSQLite', function($cordovaSQLite) {
+	document.addEventListener("deviceready", function () {
+		db = $cordovaSQLite.openDB("iremind.db");
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+	}, false);
+}]);
