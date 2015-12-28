9653ccfb0ca32511577593bf071e3a1c8f6ce984
Ivelin Penchev <ivelinpenchev@gmail.com>
2015-12-28 17:56:16 +0200

    Added location selection and removal on map

--- www/app/main.js
+++ www/app/main.js
@@ -2,22 +2,22 @@
 var app = angular.module('app', ['ngCordova', 'ui.router', 'onsen', 'app.controllers', 'app.services', 'app.directives']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-	console.log("Running");
-	$stateProvider
-		.state('main', {
-			url: "/",
-            views:{
+    console.log("Running");
+    $stateProvider
+        .state('main', {
+            url: "/",
+            views: {
                 topbar: {
-                    templateUrl: "app/shared/topbar/topbarView.html"  
+                    templateUrl: "app/shared/topbar/topbarView.html"
                 },
                 main: {
                     templateUrl: "app/components/reminders/remindersView.html",
                     controller: 'remindersController'
                 }
             }
-		})
-		.state('map', {
-			url: "/map/",
+        })
+        .state('map', {
+            url: "/map/",
             views: {
                 topbar: {
                     templateUrl: ''
@@ -27,14 +27,24 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
                     controller: 'mapController'
                 }
             }
-		})
-	$urlRouterProvider.otherwise("/");		
+        })
+    $urlRouterProvider.otherwise("/");
 }]);
 
 // Database setup
-app.run(['$cordovaSQLite', function($cordovaSQLite) {
-	document.addEventListener("deviceready", function () {
-		var db = $cordovaSQLite.openDB("iremind.db");
-        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
-	}, false);
+app.run(['$cordovaSQLite', function ($cordovaSQLite) {
+    document.addEventListener("deviceready", function () {
+        var db = $cordovaSQLite.openDB({name: "iremind.db"});
+        // Setup queries
+        var queries = [
+            "CREATE TABLE IF NOT EXISTS locations (id integer primary key, latitude real, longitude real)",
+            "CREATE TABLE IF NOT EXISTS lists (id integer primary key, location int)"
+        ];
+        queries.forEach(function (query) {
+            $cordovaSQLite.execute(db, query).then(function (res) {
+            }, function (err) {
+                console.error(err);
+            });
+        })
+    }, false);
 }]);
--- www/app/shared/map/mapDirective.js
+++ www/app/shared/map/mapDirective.js
@@ -4,28 +4,18 @@ app.directive('map', ['$cordovaGeolocation', function ($cordovaGeolocation) {
     return {
         templateUrl: "app/shared/map/mapView.html",
         link: function (scope, element, attrs) {
-            console.log(element);
-
-            var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-
+            
+            // Initialise map with center current position
             var mapOptions = {
-                center: myLatlng,
                 zoom: 16,
                 mapTypeControl: false,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
-
             var map = new google.maps.Map(element[0].children[0], mapOptions);
             var posOptions = { timeout: 10000, enableHighAccuracy: true };
             $cordovaGeolocation
                 .getCurrentPosition(posOptions)
                 .then(function (position) {
-
-
-                    console.log('asd');
-                    console.log(position.coords.latitude);
-                    console.log(position.coords.longitude);
-                    console.log('asd');
                     map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
                     var myLocation = new google.maps.Marker({
                         position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
@@ -35,12 +25,41 @@ app.directive('map', ['$cordovaGeolocation', function ($cordovaGeolocation) {
                 }, function (err) {
                     console.error(err);
                 });
-                
+
             // Create the search box and link it to the UI element.
             var input = element[0].children[1];
-            console.log(input);
             var searchBox = new google.maps.places.SearchBox(input);
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+            map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+            markers = [];
+            // Done loading and setting up map
+            
+            // Helper functions for events
+            var addCustomLocation = function (position, name) {
+                var marker = new google.maps.Marker({
+                    map: map,
+                    title: name,
+                    position: position
+                });
+                // Attach click event to remove marker from map
+                marker.addListener('click', function(a){
+                     marker.setMap(null);
+                     marker = null;
+                });
+                markers.push(marker);
+                window.localStorage['markers'] = [];
+                markers.forEach
+            }
+
+            var clearCustomLocations = function () {
+                markers.forEach(function (marker) {
+                    marker.setMap(null);
+                })
+            }
+            
+            // Click event on map for manual location
+            map.addListener('click', function (e) {
+                addCustomLocation(e.latLng, 'Custom location');
+            });
 
             // Bias the SearchBox results towards current map's viewport.
             map.addListener('bounds_changed', function () {
@@ -52,7 +71,6 @@ app.directive('map', ['$cordovaGeolocation', function ($cordovaGeolocation) {
             // more details for that place.
             searchBox.addListener('places_changed', function () {
                 var places = searchBox.getPlaces();
-
                 if (places.length == 0) {
                     return;
                 }
@@ -61,26 +79,12 @@ app.directive('map', ['$cordovaGeolocation', function ($cordovaGeolocation) {
                 markers.forEach(function (marker) {
                     marker.setMap(null);
                 });
-                markers = [];
 
                 // For each place, get the icon, name and location.
                 var bounds = new google.maps.LatLngBounds();
+                clearCustomLocations();
                 places.forEach(function (place) {
-                    var icon = {
-                        url: place.icon,
-                        size: new google.maps.Size(71, 71),
-                        origin: new google.maps.Point(0, 0),
-                        anchor: new google.maps.Point(17, 34),
-                        scaledSize: new google.maps.Size(25, 25)
-                    };
-
-                    // Create a marker for each place.
-                    markers.push(new google.maps.Marker({
-                        map: map,
-                        icon: icon,
-                        title: place.name,
-                        position: place.geometry.location
-                    }));
+                    addCustomLocation(place.geometry.location, place.name);
 
                     if (place.geometry.viewport) {
                         // Only geocodes have viewport.
