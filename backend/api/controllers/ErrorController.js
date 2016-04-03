daf06624dbb479c54d9222ee1a155c96276261d8
Ivelin Penchev <ivelinpenchev@gmail.com>
2016-04-03 17:15:12 +0100

    backend updated

---
+++ backend/api/controllers/ErrorController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    log: function(req, res) {
+        return res.send("Hi there!");
+    }
+};
--- frontend/app/pages/settings/settings.ts
+++ frontend/app/pages/settings/settings.ts
@@ -1,20 +1,36 @@
 import {Page, NavController} from 'ionic-angular';
 import {DbService} from '../../services/dbService';
 import {GeofenceService} from '../../services/geofenceService';
-import {LocationService} from "../../services/locationService";
-
+import {LocationService} from '../../services/locationService';
+import {Http} from 'angular2/http';
+import 'rxjs/add/operator/map';
 
 @Page({
     templateUrl: 'build/pages/settings/settings.html'
 })
 export class Settings {
-    constructor(private nav: NavController, private dbService: DbService, private geofenceService: GeofenceService, private locationService: LocationService) { }
+    constructor(
+        private nav: NavController,
+        private dbService: DbService,
+        private geofenceService: GeofenceService,
+        private locationService: LocationService,
+        private http: Http) { }
 
     wipe() {
         this.dbService.wipe();
     }
-    
+
     sync() {
         this.geofenceService.sync();
+    }
+
+    test() {
+        let body = JSON.stringify({ name: 'asd' });
+
+        return this.http.post("http://82.25.173.240:1337/error", body).map(res => res.json()).subscribe(
+            data => console.log(data),
+            err => console.error(err),
+            () => console.log('Movie Search Complete')
+        );
     }
 }
